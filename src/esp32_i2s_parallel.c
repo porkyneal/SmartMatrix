4aa60810c6adf25099edfc038ed706628ea17f2c
Aaron N <porkyneal@gmail.com>
2022-02-24 17:24:14 +0000

    Update esp32_i2s_parallel.c

--- src/esp32_i2s_parallel.c
+++ src/esp32_i2s_parallel.c
@@ -31,6 +31,9 @@
 #include "rom/lldesc.h"
 #include "esp_heap_caps.h"
 #include "esp32_i2s_parallel.h"
+#include "soc/gpio_periph.h"
+#include "hal/gpio_types.h"
+#include "esp32-hal.h"
 
 typedef struct {
     volatile lldesc_t *dmadesc_a, *dmadesc_b;
